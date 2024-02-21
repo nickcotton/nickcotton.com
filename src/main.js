4f04ded60275d2fa56aaf53c95f04ab4e8df304e
Nick Cotton <nick.c.cotton@gmail.com>
2024-02-21 21:57:48 +0000

    chore: remove sw.js

--- src/main.js
+++ src/main.js
@@ -10,17 +10,3 @@ Alpine.plugin(persist);
 document.addEventListener("alpine:init", () => {});
 
 Alpine.start();
-
-const env = document.querySelector("body").dataset.env;
-
-// Check that service workers are supported
-if ("serviceWorker" in navigator && env === "production") {
-  // use the window load event to keep the page load performant
-  window.addEventListener("load", () => {
-    try {
-      navigator.serviceWorker.register("/sw.js");
-    } catch (error) {
-      console.error("Service worker registration failed: ", error);
-    }
-  });
-}
