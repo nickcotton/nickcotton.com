0df60911752a50062280116506578dd0c872d974
Nick Cotton <nick.c.cotton@gmail.com>
2024-03-06 21:52:15 +0000

    feat: 3d phone on projects page

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import "./styles/main.css";
 
 import { Alpine } from "alpinejs";
 import { persist } from "@alpinejs/persist";
+import { Application } from "@splinetool/runtime";
 
 window.Alpine = Alpine;
 
@@ -10,3 +11,7 @@ Alpine.plugin(persist);
 document.addEventListener("alpine:init", () => {});
 
 Alpine.start();
+
+const canvas = document.getElementById("canvas3d");
+const app = new Application(canvas);
+app.load("https://prod.spline.design/hVsrYOWgXz-v8CVc/scene.splinecode");
