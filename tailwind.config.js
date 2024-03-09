40cad1d23e4fb61a09ab02bfcdcde536c894d1f0
Nick Cotton <nick.c.cotton@gmail.com>
2024-03-09 20:59:29 +0000

    feat: add basic pop-in animation to light/dark button

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,15 @@ module.exports = {
         sans: ["Inter var", ...defaultTheme.fontFamily.sans],
         serif: ["Dropkicker", ...defaultTheme.fontFamily.serif],
       },
+      keyframes: {
+        pop: {
+          "0%": { transform: "scale(0.5)" },
+          "100%": { transform: "scale(1)" },
+        },
+      },
+      animation: {
+        pop: "pop 0.5s ease-out",
+      },
     },
   },
   variants: {},
