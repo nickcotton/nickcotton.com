e5dc93a60fa7098e89e186a9e2b4edfae70f80ea
Nick Cotton <nick.c.cotton@gmail.com>
2024-04-11 07:04:15 +0100

    fix: try deploy_url

--- src/_data/site.js
+++ src/_data/site.js
@@ -1,6 +1,6 @@
 export default {
   name: "Nick Cotton",
-  url: process.env.BASE_URL || "http://localhost:8081",
+  url: process.env.DEPLOY_URL || "http://localhost:8081",
   meta: {
     title: "Nick Cotton",
     desc: "The website of Nick Cotton, a software engineer from Australia, based in London.",
