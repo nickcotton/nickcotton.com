b747c864df912589bf217214a5e590753a8580ce
Nick Cotton <nick.c.cotton@gmail.com>
2024-04-11 06:30:54 +0100

    feat: add base url based on env to rss feed

---
+++ src/_data/site.js
@@ -0,0 +1,8 @@
+export default {
+  name: "Nick Cotton",
+  url: process.env.BASE_URL || "http://localhost:8081",
+  meta: {
+    title: "Nick Cotton",
+    desc: "The website of Nick Cotton, a software engineer from Australia, based in London.",
+  },
+};
