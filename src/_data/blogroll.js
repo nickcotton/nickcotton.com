3eb2124181bebcd0fdb3899f6982f606d4dca8eb
Nick Cotton <nick.c.cotton@gmail.com>
2024-04-12 09:59:03 +0100

    feat: add a blogroll to blog sidebar

---
+++ src/_data/blogroll.js
@@ -0,0 +1,57 @@
+export default [
+  {
+    name: "Arun Venkatesan",
+    url: "https://arun.is",
+    feed: "https://arun.is/feed.xml",
+  },
+  {
+    name: "Anh",
+    url: "https://anhvn.com/",
+    feed: "https://anhvn.com/feed.xml",
+  },
+  {
+    name: "Brett Terpstra",
+    url: "https://brettterpstra.com/",
+    feed: "https://brettterpstra.com/atom.xml",
+  },
+  {
+    name: "Matt Birchler",
+    url: "https://birchtree.me/",
+    feed: "https://birchtree.me/rss/",
+  },
+  {
+    name: "Hey Dingus",
+    url: "https://heydingus.net/",
+    feed: "https://heydingus.net/feed.rss",
+  },
+  {
+    name: "Josh Comeau",
+    url: "https://www.joshwcomeau.com/",
+    feed: "https://www.joshwcomeau.com/rss.xml",
+  },
+  {
+    name: "Khaled Abou Alfa",
+    url: "https://www.kaa.bz/",
+    feed: "https://www.kaa.bz/feed.xml",
+  },
+  {
+    name: "Nick Heer",
+    url: "https://pxlnv.com/",
+    feed: "https://pxlnv.com/feed/",
+  },
+  {
+    name: "Picalilli",
+    url: "https://piccalil.li/",
+    feed: "https://piccalil.li/feed.xml",
+  },
+  {
+    name: "Robb Knight",
+    url: "https://rknight.me/",
+    feed: "https://rknight.me/subscribe/posts/atom.xml",
+  },
+  {
+    name: "Steph Ango",
+    url: "https://stephango.com/",
+    feed: "https://stephango.com/feed.xml",
+  },
+];
