45bb2c4ffaaaddf8703605201a34f06e85f7590f
Nick Cotton <nick.c.cotton@gmail.com>
2024-10-06 10:29:43 +0100

    chore: update eslint and related deps (#192)
    * chore: update eslint and related deps
    
    * fix: re-add eslint plugin import
    
    * fix: eslint working with flat config
    
    * chore: move some eslint config

---
+++ eslint.config.js
@@ -0,0 +1,52 @@
+import globals from "globals";
+import js from "@eslint/js";
+import importPlugin from "eslint-plugin-import";
+import json from "@eslint/json";
+import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
+import pluginPromise from "eslint-plugin-promise";
+
+const jsConfig = {
+  files: ["**/*.js"],
+  ...js.configs.recommended,
+  ...importPlugin.flatConfigs.recommended,
+  ...pluginPromise.configs["flat/recommended"],
+};
+
+const jsonConfig = {
+  files: ["**/*.json"],
+  ignores: ["package-lock.json"],
+  language: "json/json",
+  ...json.configs.recommended,
+};
+
+export default [
+  jsConfig,
+  jsonConfig,
+  {
+    ignores: ["**/dist", "**/_site", "!**/.stylelintrc.json"],
+  },
+  {
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+
+      ecmaVersion: "latest",
+      sourceType: "module",
+
+      parserOptions: {
+        allowImportExportEverywhere: true,
+
+        ecmaFeatures: {
+          impliedStrict: true,
+        },
+      },
+    },
+
+    settings: {
+      "import/external-module-folders": ["node_modules", "web_modules"],
+    },
+  },
+  eslintPluginPrettierRecommended,
+];
